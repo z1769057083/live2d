2dd2c1a14c9b22e73f714698be47535d92dbb92e
lmaomaoz <727559597@qq.com>
2024-10-10 20:21:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-loadlive2d("live2d", "./live2d/model/tia/model.json");
+window.addEventListener("load", function () {
+    if (document.getElementById("live2d")) {
+        return;
+    }
+    var canvas = document.createElement("canvas");
+    canvas.id = "live2d";
+    canvas.width = "280";
+    canvas.height = "250";
+    canvas.className = "live2d";
+    canvas.style = `position:fixed;right:0;bottom:0;`;
+    document.body.appendChild(canvas);
+    loadlive2d("live2d", "./live2d/model/tia/model.json");
+});
